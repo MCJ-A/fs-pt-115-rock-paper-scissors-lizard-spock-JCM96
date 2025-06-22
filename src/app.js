ce94f4a26a91c931c2fe8ec6f1c5ff4f99a34e99
Jhon Cordova <jhon.a.cordova.martinez@gmail.com>
2025-06-22 19:24:00 +0000

    PPTLS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+// Usamos los nombres en inglés como se especifica en el README para consistencia.
+const OPTIONS = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+/**
+ * La computadora azar.
+ */
+const getComputerChoice = () => {
+  const index = Math.floor(Math.random() * OPTIONS.length);
+  return OPTIONS[index];
+};
 
+/**
+ * Comparar las jugadas y resultado.
+ */
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "empate!";
+  }
+    // Reglas para saber quién gana a quién
+  const rules = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['paper', 'spock'],
+    spock: ['scissors', 'rock'],
+  };
+    if (rules[userChoice].includes(computerChoice)) {
+    return 'ganaste 🎉';
+  }
+    return 'perdiste 😢';
+};
+// --- Inicia el juego ---
+const userChoice = prompt(`Choose one of: ${OPTIONS.join(', ')}: `).toLowerCase();
+
+if (OPTIONS.includes(userChoice)) {
+  const computerChoice = getComputerChoice();
+    console.log(`User chose: ${userChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+  console.log(determineWinner(userChoice, computerChoice));
+} else {
+  console.log('opcion invalida');
+}
